9da9d6208a7d2f6f24b98490b0da13c4c6af1749
DavidCM <dcm.robin@gmail.com>
2023-07-28 13:36:33 +0100

    going to try something

--- Assets/Scripts/Sickness.cs
+++ Assets/Scripts/Sickness.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Sickness : MonoBehaviour
+public class Sickness
 {
     public int sicknessID;
     public string sicknessName;
